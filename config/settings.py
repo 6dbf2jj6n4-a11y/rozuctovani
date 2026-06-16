8f13ada09864a3911a4d4f4a9d6285f46c22a4fd
RenteX <6dbf2jj6n4@privaterelay.appleid.com>
2026-06-16 17:08:42 +0200

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -66,6 +66,11 @@ if os.environ.get("DJANGO_DB_ENGINE") == "sqlite3":
             "NAME": BASE_DIR / "db.sqlite3",
         }
     }
+elif os.environ.get("DATABASE_URL"):
+    import dj_database_url
+    DATABASES = {
+        "default": dj_database_url.config(conn_max_age=600, ssl_require=False)
+    }
 else:
     DATABASES = {
         "default": {
