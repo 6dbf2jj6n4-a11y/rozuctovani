8ee6e76568eef8558810d0a6f5a47a7544787975
RenteX <6dbf2jj6n4@privaterelay.appleid.com>
2026-06-17 16:02:58 +0200

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -14,6 +14,7 @@ ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS", "*").split(",")
 
 
 INSTALLED_APPS = [
+    "unfold",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
