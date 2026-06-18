07548df270e6694cec20f0bfc02ac825773bef6a
RenteX <6dbf2jj6n4@privaterelay.appleid.com>
2026-06-18 14:00:32 +0200

    Update 0005_clientcard_fields.py

--- core/migrations/0005_clientcard_fields.py
+++ core/migrations/0005_clientcard_fields.py
@@ -1,4 +1,4 @@
-from django.db import migrations, models
+from django.db import migrations
 
 
 class Migration(migrations.Migration):
@@ -7,25 +7,4 @@ class Migration(migrations.Migration):
         ("core", "0004_clientcard_unit_nullable"),
     ]
 
-    operations = [
-        migrations.AddField(
-            model_name="unit",
-            name="code",
-            field=models.CharField(blank=True, max_length=50, verbose_name="Kód / označení"),
-        ),
-        migrations.AddField(
-            model_name="unit",
-            name="purpose",
-            field=models.CharField(blank=True, max_length=100, verbose_name="Účel"),
-        ),
-        migrations.AddField(
-            model_name="unit",
-            name="rate_per_m2_year",
-            field=models.DecimalField(blank=True, decimal_places=2, max_digits=10, null=True, verbose_name="Sazba Kč/m²/rok"),
-        ),
-        migrations.AddField(
-            model_name="unit",
-            name="unit_type",
-            field=models.CharField(blank=True, default="m2", max_length=10, verbose_name="Jednotka"),
-        ),
-    ]
+    operations = []
