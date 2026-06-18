2d80129e0a98651d334bf0806208afbd5db4a6e2
RenteX <6dbf2jj6n4@privaterelay.appleid.com>
2026-06-18 12:04:14 +0200

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -126,3 +126,23 @@ UNFOLD = {
     "SITE_TITLE": "Rozúčtování",
     "SITE_HEADER": "Správa rozúčtování",
 }
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler",
+        },
+    },
+    "root": {
+        "handlers": ["console"],
+        "level": "DEBUG",
+    },
+    "loggers": {
+        "django": {
+            "handlers": ["console"],
+            "level": "DEBUG",
+            "propagate": False,
+        },
+    },
+}
