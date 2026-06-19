a740b733225c96af7ee1252409405b1c7ee83514
RenteX <6dbf2jj6n4@privaterelay.appleid.com>
2026-06-19 10:17:48 +0200

    Update cardunit_autofill.js

--- core/static/core/js/cardunit_autofill.js
+++ core/static/core/js/cardunit_autofill.js
@@ -2,33 +2,51 @@
 Pri vyberu plochy (Unit) v inline "Plochy a najemne" automaticky
 vyplni pole "Vymera (m2) - uprava" hodnotou z zasobniku (Unit.area_m2),
 pokud je pole prazdne (aby se neprepisovaly rucni upravy).
+
+Pouziva django.jQuery, protoze autocomplete pole (select2) generuje
+zmenu hodnoty pres jQuery udalosti, ktere nezachyti standardni
+document.addEventListener('change', ...).
 */
-document.addEventListener("change", function (e) {
-    if (!e.target.matches('select[id$="-unit"]')) {
-        return;
-    }
-    var row = e.target.closest("tr");
-    if (!row) {
-        return;
-    }
-    var areaInput = row.querySelector('input[id$="-area_m2_override"]');
-    if (!areaInput || areaInput.value) {
-        return;
-    }
-    var unitId = e.target.value;
-    if (!unitId) {
-        return;
-    }
-    fetch("/admin/core/unit/area-lookup/" + unitId + "/")
-        .then(function (response) {
-            return response.json();
-        })
-        .then(function (data) {
-            if (data.area !== null && data.area !== undefined) {
-                areaInput.value = data.area;
+(function () {
+    function attach($) {
+        $(document).on("select2:select change", 'select[id$="-unit"]', function () {
+            var $select = $(this);
+            var $row = $select.closest("tr");
+            if ($row.length === 0) {
+                return;
             }
-        })
-        .catch(function () {
-            /* ticho - pokud lookup selze, pole jednoduse zustane prazdne */
+            var $areaInput = $row.find('input[id$="-area_m2_override"]');
+            if ($areaInput.length === 0 || $areaInput.val()) {
+                return;
+            }
+            var unitId = $select.val();
+            if (!unitId) {
+                return;
+            }
+            fetch("/admin/core/unit/area-lookup/" + unitId + "/")
+                .then(function (response) {
+                    return response.json();
+                })
+                .then(function (data) {
+                    if (data.area !== null && data.area !== undefined) {
+                        $areaInput.val(data.area);
+                    }
+                })
+                .catch(function () {
+                    /* ticho - pokud lookup selze, pole jednoduse zustane prazdne */
+                });
         });
-});
+    }
+
+    if (window.django && window.django.jQuery) {
+        attach(window.django.jQuery);
+    } else if (window.jQuery) {
+        attach(window.jQuery);
+    } else {
+        document.addEventListener("DOMContentLoaded", function () {
+            if (window.django && window.django.jQuery) {
+                attach(window.django.jQuery);
+            }
+        });
+    }
+})();
