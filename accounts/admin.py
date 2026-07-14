7628dc2447e54d56d7e1a9a2809100dd7ed9ed42
RenteX <6dbf2jj6n4@privaterelay.appleid.com>
2026-07-14 17:51:32 +0200

    Unregister default User model in admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User as AuthUser
 from unfold.admin import ModelAdmin
 from .models import User
+
+try:
+    admin.site.unregister(AuthUser)
+except admin.sites.NotRegistered:
+    pass
 
 
 @admin.register(User)
