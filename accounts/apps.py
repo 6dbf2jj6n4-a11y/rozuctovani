27bb896d3692c23bef287a7a334b33fed01a1096
RenteX <6dbf2jj6n4@privaterelay.appleid.com>
2026-07-14 18:02:21 +0200

    Update apps.py

--- accounts/apps.py
+++ accounts/apps.py
@@ -4,4 +4,4 @@ from django.apps import AppConfig
 class AccountsConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "accounts"
-    verbose_name = "Uzivatele"
+    verbose_name = "Správa přístupu"
