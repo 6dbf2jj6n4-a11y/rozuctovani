dc12d89af526d6db277d1a27d5a8ec737b850ba3
Daniel David <daniel.david@me.com>
2026-08-08 10:05:06 +0200

    Pridat docasnou diagnostiku poctu DB dotazu na pomale requesty
    Stranka Položky (core_billingline_changelist) se nacita >6s bez
    zjevne pricinny v kodu (list_select_related uz je nastaveny, zadne
    dalsi dotazy v list_display metodach) - SlowRequestLoggingMiddleware
    zaloguje cestu/dobu/POCET dotazu (ne SQL text) pro kazdy request >1s,
    aby se dalo presne zjistit, kde se cas ztraci, misto dalsiho hadani.

--- config/middleware.py
+++ config/middleware.py
@@ -1,4 +1,37 @@
+import logging
+import time
+
+from django.db import connection
 from django.shortcuts import redirect
+from django.test.utils import CaptureQueriesContext
+
+logger = logging.getLogger("perf")
+
+
+class SlowRequestLoggingMiddleware:
+    """Docasna diagnostika: zaloguje cestu, dobu trvani a POCET DB dotazu
+    (ne jejich SQL text) pro kazdy pomaly (>1s) request od prihlaseneho
+    uzivatele - bez zapnuti plneho SQL logovani, ktere uz bylo vypnuto
+    kvuli zahlcovani produkcnich logu (viz DEBUG=0, commit 83fe24c).
+    CaptureQueriesContext funguje nezavisle na DEBUG (docasne zapne
+    force_debug_cursor jen pro tenhle request). Pouzito k dohledani,
+    KTERA stranka dela kolik dotazu - viz konverzace s Danielem,
+    stranka "Položky" se nacita >6s bez zjevne pricinny v kodu."""
+
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        start = time.monotonic()
+        with CaptureQueriesContext(connection) as ctx:
+            response = self.get_response(request)
+        duration = time.monotonic() - start
+        if duration > 1 and getattr(request, "user", None) and request.user.is_authenticated:
+            logger.warning(
+                "POMALY REQUEST: %s %s - %.2fs, %d DB dotazu",
+                request.method, request.path, duration, len(ctx.captured_queries),
+            )
+        return response
 
 
 class NonStaffAdminRedirectMiddleware:
--- config/settings.py
+++ config/settings.py
@@ -40,6 +40,7 @@ MIDDLEWARE = [
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "config.middleware.SlowRequestLoggingMiddleware",
     "config.middleware.NonStaffAdminRedirectMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
